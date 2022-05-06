39bb0f38f5e008972b6cd55023f586b74d2ba134
Rui <rui.crispim@yahoo.com.br>
2022-05-05 23:54:34 -0300

    Organização das pastas do projeto e tradução das classes  para inglês

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express= require("express");
-const rotasProdutos = require("./routes/rotasProdutos");
+const rotasProdutos = require("./src/routes/products.route");
 let app = express();
 
 
 app.get("/",(req,res)=>res.send("Olá Mundo!"));
 app.get("/contatos",(req,res)=>res.send("Pagina contatos"));
 
-app.use("/produtos", rotasProdutos);
+app.use("/products", rotasProdutos);
 
 // Criando um servidor
 app.listen(4000,()=>console.log("Servidor rodando na porta 4000"))
