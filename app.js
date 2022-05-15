d2a7d2ab8da7502ec2739d2f9cbedba3524166f9
Rui <rui.crispim@yahoo.com.br>
2022-05-15 14:05:26 -0300

    Alterando caminho da pasta views para src/views

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var usedProductsRouter= require('./src/routes/used')
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
+app.set('views',path.join(__dirname,'/src/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
