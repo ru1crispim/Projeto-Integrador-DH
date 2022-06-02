91b7e59988438393e03c8fd6b2a397fd83f2fe56
Rui <rui.crispim@yahoo.com.br>
2022-06-02 20:33:27 -0300

    Criação da pagina de manutençao do site com cadastro/alteracao de produtos

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const req = require("express/lib/request");
 const res = require("express/lib/response");
 const router = express.Router();
 const productController = require('../controllers/productController');
@@ -6,11 +7,19 @@ const productController = require('../controllers/productController');
 const logDBMiddleware = require('../middlewares/logDB')
 
 router.get('/allproducts',productController.products);
-router.post('/product',productController.create);
-router.post('/product/:id',productController.update); //fazer put após método override
-router.get('/productform', (req, res)=>{
-    return res.render("productForms")
+router.get('/product/settings',(req,res)=>{res.render('productSettings')});
+
+
+router.get('/productform/registration', (req, res)=>{
+    return res.render("productFormRegistration")
 })
+router.get('/productform/update', (req, res)=>{
+    return res.render("productFormUpdate")
+})
+
+router.post('/createproduct',productController.create);
+router.put('/updateproduct',productController.update);
+
 
 
 module.exports=router;
