95c8333d853c51b500d4ed952266d1d0a8db24dc
Rui <rui.crispim@yahoo.com.br>
2022-06-03 22:58:41 -0300

    Criação dos formularios de produtos e aplicação do multer / Criação do arquivo Users.Model.js para o Danilo /

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,6 +3,8 @@ const req = require("express/lib/request");
 const res = require("express/lib/response");
 const router = express.Router();
 const productController = require('../controllers/productController');
+const path = require('path');
+const multer = require('multer');
 
 const logDBMiddleware = require('../middlewares/logDB')
 
@@ -17,9 +19,27 @@ router.get('/productform/update', (req, res)=>{
     return res.render("productFormUpdate")
 })
 
-router.post('/createproduct',productController.create);
-router.put('/updateproduct',productController.update);
 
+// Rota para cadastro de produto + envio de arquivo (Multer)
+
+const multerStorage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        const folder = path.join(__dirname,'../public/productsImg');
+        cb(null,folder);
+    },
+    filename:(req,file,cb)=>{
+        const imageName = Date.now() +file.originalname;
+        cb(null,imageName);
+    }
+});
+const upload = multer({storage:multerStorage})
+
+router.post('/createproduct',upload.single('imgJogo'),productController.create);
+
+// Rotas para visualização e atualização de produtos
+
+router.put('/updateproduct',productController.update);
+router.get('/product/view',productController.view);
 
 
 module.exports=router;
