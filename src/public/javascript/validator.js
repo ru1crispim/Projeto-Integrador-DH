424ea0cd59ff68c29258eb15a7ec5129ab7b9a3d
GBicaleto <gabriel.bicaleto@gmail.com>
2022-08-28 19:12:58 -0300

    feito atualizações de html e css em algumas view e inicio de validação form

--- src/public/javascript/validator.js
+++ src/public/javascript/validator.js
@@ -2,39 +2,36 @@ window.addEventListener('load', function() {
    
     let formCadastrase = document.querySelector('form.formCadastrase');
 
-    formCadastrase.addEventListener('submit', function(event) {
+    let fieldName = document.getElementById('name');
+    let fieldEmail = document.getElementById('email');
+    let fieldPassword = document.getElementById('senha');
 
+
+    // verificações antes de enviar dados
+    formCadastrase.addEventListener('submit', function(event) {
         let errors = [];
 
-        let fieldName = document.getElementById('name');
             if (fieldName.value == ''){
-                alert('O Campo nome não pode estar vazio')
-                errors.push('O Campo nome não pode estar vazio')
+                errors.push("O Campo nome não pode estar vazio.\n");
             } else if (fieldName.value.length < 3) {
-                alert('O Campo nome precisa ter mais de 3 caracteres')
-                errors.push('O Campo nome precisa ter mais de 3 caracteres')
+                errors.push('O Campo nome precisa ter mais de 3 caracteres.\n');
             };
 
-       let fieldEmail = document.getElementById('login');
             if (fieldEmail.value == ''){
-                alert('O Campo E-mail não pode estar vazio')
-                errors.push('O Campo E-mail não pode estar vazio')
+                errors.push('O Campo E-mail não pode estar vazio.\n');
             } else if (fieldEmail.value.length < 11) {
-                alert('Campo E-mail invalido!')
-                errors.push('Campo E-mail invalido!')
-            };
+                errors.push('Campo E-mail invalido!.\n');
+            }
 
-        let fieldPassword = document.getElementById('senha');
             if (fieldPassword.value == ''){
-                alert('O Campo senha não pode estar vazio')
-                errors.push('O Campo senha não pode estar vazio')
+                errors.push('O Campo senha não pode estar vazio.\n');
             } else if (fieldPassword.value.length < 6) {
-                alert('A senha tem que ter mais que 5 caracteres')
-                errors.push('A senha tem que ter mais que 5 caracteres')
+                errors.push('A senha tem que ter mais que 5 caracteres.\n');
             };
             
            if (errors.length > 0){
             event.preventDefault();
+            alert(errors);
            }
         
     })
